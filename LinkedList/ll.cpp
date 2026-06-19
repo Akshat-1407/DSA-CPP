bfa29af39ab0488624a7b5bb985b05295a900fe8
Akshat-1407 <akshat.rastogi1407@gmail.com>
2026-06-19 21:04:20 +0530

    ...

--- LinkedList/ll.cpp
+++ LinkedList/ll.cpp
@@ -3,36 +3,78 @@
 using namespace std;
 
 class Node {
-    public:
+public:
     int data;
     Node* next;
 
     Node(int data) {
         this->data = data;
-        this->next = NULL;
+        this->next = nullptr;
+    }
+
+    Node(int data, Node* next) {
+        this->data = data;
+        this->next = next;
     }
 };
 
-void insertAtHead(Node* &head, int data) {
-    Node* temp = new Node(data);
-    temp->next = head;
-    head = temp;
-}
+class List {
+private:
+    Node* head;
 
-void print(Node* &head) {
-    Node* temp = head;
-    while (temp->next != NULL) {
-        cout << temp->data << " ";
-        temp = temp->next;
+public:
+    List() {
+        head = nullptr;
     }
-}
+
+    void insertAtHead(int data) {
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void deleteAtHead() {
+        if (head == nullptr) 
+            return;
+
+        Node* temp = head;
+        head = temp->next;
+        delete temp;
+    }
+
+    void display() {
+        Node* temp = head;
+
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+
+        cout << "NULL\n";
+    }
+
+    
+
+    ~List() {
+        while (head != nullptr) {
+            deleteAtHead();
+        }
+    }
+
+};
+
+
 
 int main() {
 
-    Node* n = new Node(10);
-    print(n);
+    List list;
 
-    // insertAtHead()
+    list.insertAtHead(2);
+    list.insertAtHead(4);
+    list.insertAtHead(6);
+    list.insertAtHead(8);
+
+    list.display();
 
     return 0;
 }
